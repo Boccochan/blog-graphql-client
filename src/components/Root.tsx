021002c37a9caa89258f060a6d8c37dba500439e
Boccochan <boccochan@gmail.com>
2020-04-05 13:37:44 +0900

    Apply navbar to all pages

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import PrimarySearchAppBar from "./Menu";
 
 export default function Root() {
   return (
     <div>
-      <PrimarySearchAppBar />
       <h1>This Root!!</h1>
     </div>
   );
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -4,11 +4,13 @@ import React, { Component } from "react";
 // import SecondPage from "./SecondPage";
 import Post from "./Post";
 import Root from "./Root";
+import PrimarySearchAppBar from "./Menu";
 
 export default class Routes extends Component {
   render() {
     return (
       <div>
+        <PrimarySearchAppBar />
         <Switch>
           <Route exact={true} path="/" component={Root} />
           <Route path="/post" component={Post} />
